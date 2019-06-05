96d156dbfc787a1d79ccd084921f35fa3f2c1bd7
John Read <john@datasciencehealth.com>
2019-06-04 21:14:00 -0700

    add pie

--- static/js/pie.js
+++ static/js/pie.js
@@ -14,9 +14,8 @@ setTimeout(function(){
     grades.push(0);
     grades.push(0);
     grades.push(0);
-    grades.push(0);
     var pieColor = ['#0f1ddd', '#46a346', '#dd0f18'];
-    var labels = ["A Rating", "B Rating", "C Rating", "Other"]
+    var labels = ["A Rating", "B Rating", "C Rating"];
     for(var j = 0; j < restgrade.length; j++){
         // grades.push(j);
         // console.log(restgrade[j]);
@@ -29,11 +28,8 @@ setTimeout(function(){
         else if(restgrade[j] == "B"){
             grades[1] = grades[1]+1;
         }
-        else if(restgrade[j] == "C"){
-            grades[2] = grades[2]+1;           
-        }
         else{
-            grades[3] = grades[3]+1;   
+            grades[2] = grades[2]+1;   
         }
     }
     console.log(grades);
